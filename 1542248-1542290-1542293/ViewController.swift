76247b870ef9eac280599dbc870bdec79be64bd1
Than Hoang Xuan Nghiep <thanhoangxuannghiep@Thans-Mac.local>
2017-04-08 10:52:04 +0700

    add basic project

---
+++ 1542248-1542290-1542293/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  1542248-1542290-1542293
+//
+//  Created by Than Hoang Xuan Nghiep on 4/8/17.
+//  Copyright © 2017 Than Hoang Xuan Nghiep. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
